2d70cd272c4139cfd9d6ea0b55dc81dbf23d602e
boonsongH1 <Natthaphol.khao@bumail.net>
2025-05-06 00:05:31 +0700

    Jump and Ground

--- Assets/New Folder/Player Movement.cs
+++ Assets/New Folder/Player Movement.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -8,7 +9,7 @@ public class PlayerMovement : MonoBehaviour
         
     //Jump
     [SerializeField] float jumpForce;
-    
+    [SerializeField] private bool isjumping;
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -19,8 +20,34 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Walk
         move = Input.GetAxis("Horizontal");
 
         rb2d.linearVelocity = new Vector2(move * speed, rb2d.linearVelocity.y);
+        
+        //Jump
+        if (Input.GetButtonDown("Jump") && !isjumping)
+        {
+            rb2d.AddForce(new Vector2(rb2d.linearVelocity.x , jumpForce));
+            Debug.Log("Jump!");
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isjumping = false;
+        }
+        
+    }
+    
+
+    private void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Ground"))
+        {
+            isjumping = true;
+        }
     }
 }
